87983e419a7a8ebe968471e6170a382e832fbc3c
Pranav Verma <vermapranav86@gmail.com>
2020-11-03 15:19:10 +0530

    Create GascostForint8.sol

---
+++ day4/GascostForint8.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.6;
+contract  GasCostforint8{
+    int8 i=100;
+  
+}
+
+/* transaction hash	0x09579ee74587041d58963c9d0d2d293bfae30b98c055860273c669db56817479 
+ contract address	0x8431717927c4a3343bcf1626e7b5b1d31e240406 
+ from	0xab8483f64d9c6d1ecf9b849ae677dd3315835cb2 
+ to	GasCostforint8.(constructor) 
+ gas	3000000 gas 
+ transaction cost	91517 gas 
+ execution cost	31541 gas 
+ hash	0x09579ee74587041d58963c9d0d2d293bfae30b98c055860273c669db56817479 
+ input	0x608...f0029 */
