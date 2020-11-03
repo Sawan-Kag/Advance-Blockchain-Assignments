43a1ecea8bf5a0c8bee06ef622abfda147b7b450
Pranav Verma <vermapranav86@gmail.com>
2020-11-03 15:22:10 +0530

    Create GasCostforint256.sol

---
+++ day4/GasCostforint256.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.4.6;
+contract  GasCostforint256{
+    int256 i=10000;
+  
+}
+
+/* 
+
+
+ status	0x1 Transaction mined and execution succeed
+ transaction hash	0x7ff1ac2293ecd74ec9d2301ada273a95a7e42b314415e33bc51c0e90ceab0ad1 
+ contract address	0x09943fa8dd32c76f7b880627a0f6af73e8f5a595 
+ from	0xab8483f64d9c6d1ecf9b849ae677dd3315835cb2 
+ to	GasCostforint256.(constructor) 
+ gas	3000000 gas 
+ transaction cost	89144 gas 
+ execution cost	30672 gas 
+ hash	0x7ff1ac2293ecd74ec9d2301ada273a95a7e42b314415e33bc51c0e90ceab0ad1 
+ input	0x608...b0029 
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+*/
